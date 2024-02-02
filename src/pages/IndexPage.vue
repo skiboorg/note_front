6a491b2ce6574fbceda8cbe7a3014ef847d31ceb
xxx <dd@dd.dd>
2024-02-02 15:01:05 +0300

    2

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -17,60 +17,97 @@
 
         </q-drawer>
 
-    <div class="top mb-100">
+    <div class="top ">
     <div class="container">
       <div class="header flex items-center justify-between mb-100">
         <div class="logo">
           <img src="~assets/yellow/logo.png" alt="">
-          <p class="no-margin">
+          <p class="no-margin text-negative text-28">
             bitcoin<br>keepers
           </p>
         </div>
-        <p class="no-margin text-32 gt-sm">3 33  333</p>
-        <q-btn no-caps unelevated color="black" text-color="grey-8" class="no-border-radius btn gt-sm" label="Connect wallet"/>
+        <p class="no-margin text-32 gt-sm text-negative">3 33  333</p>
+        <q-btn no-caps unelevated color="negative" text-color="black" size="20px" class="no-border-radius btn gt-sm" label="Connect wallet"/>
         <q-btn icon="menu" @click="leftDrawerOpen=true" flat round text-color="dark" class="lt-md"/>
       </div>
     </div>
 
     <div class="container row q-col-gutter-md items-center">
 
-      <div class="col-12 col-md-6 ">
-        <p class="text-64">NO Ut1l1ty.</p>
-        <p class="text-28">The Keepers are not just a set of numbers and pixels. We set ourselves the task of building a large, strong community, each of whose members soberly assesses the market and its prospects, understands the concept of its structure, and contributes to its development. The basis we want to lay in the foundation of such a community is absolute openness in our plans and actions, it is trust. It all starts from there.</p>
+      <div class="col-12 col-md-7 ">
+        <p class="text-64 text-white">k33p3rs 3ncrypt0r</p>
+        <p class="text-24 text-bitmgothic text-white">3ns3rt a k3y t0 3volv3...</p>
+        <p class="text-28 text-negative text-bitmgothic">
+          The Keepers are not just a set of numbers and pixels.
+          We set ourselves the task of building a large,strong community, each of whose members soberly assesses the market and its prospects, understands the concept of web3 structure and contributes to its development.<br>
+          The basis we want to lay in the foundation of such a community is absolute openness in our plans and actions. We don't make lofty promises and don't make people wait for things we can't or don't want to do;
+          we simply take action and demonstrate it.
+          It all starts from trust.
+        </p>
       </div>
-      <div class="col-12 col-md-5 offset-0 offset-md-1 ">
+      <div class="col-12 col-md-5 ">
         <q-img src="~assets/yellow/main-1.png" alt=""/>
       </div>
     </div>
 
   </div>
-    <div class="container row q-col-gutter-md items-center mb-150">
-      <div class="col-12 col-md-6 ">
-        <p class="text-64 mb-100 text-white">D3ADSCROLLS</p>
-        <p class="text-64 text-white">t0 h1d3 c0nv3rsat10ns fr0m sly d3vs</p>
+    <div class="  pp">
+      <div class="container row q-col-gutter-md items-center">
+        <div class="col-12 col-md-6 ">
+          <p class="text-64 text-bitmgothic text-white">k33p3rs 3ncrypt0r</p>
+          <p class="text-24 text-bitmgothic text-white">d3adscr0lls.</p>
+          <p class="text-28 text-bitmgothic text-negative">
+            Keepers enter the blocktrans, where they connect with the blockchain and leave runes on it. Runes can be decrypted and recorded in scrolls, using a language that is understandable and familiar to everyone.<br><br>
+            The secret should always remain a secret. The recorded scrolls can be read only once, by the first person to unwrap them. Afterward, the scroll burns
+
+          </p>
+        </div>
+        <div class="col-12 col-md-5 offset-0 offset-md-1">
+          <q-img src="~assets/yellow/main-2.png" alt=""/>
+        </div>
       </div>
-      <div class="col-12 col-md-5 offset-0 offset-md-1">
-        <q-img src="~assets/yellow/main-2.png" alt=""/>
-      </div>
+
     </div>
-    <div class="container bg-black mb-200 form">
-      <div class="flex items-center justify-between mb-50">
-        <p class="no-margin text-32 cursor-pointer" @click="tab='find'" :class="tab === 'find' ? 'text-white' : 'text-negative'">enter d3adscroll</p>
-        <p class="no-margin text-32 cursor-pointer" @click="makeid(), tab='create'" :class="tab === 'create' ? 'text-white' : 'text-negative'">Cr3ate d3adscroll</p>
-      </div>
+    <div class="container bg-black  form">
+      <q-btn-toggle
+        v-model="tab"
+
+
+        rounded
+        :ripple="false"
+
+        toggle-color="white"
+        toggle-text-color="dark"
+        text-color="negative"
+        color="grey-9"
+        size="26px"
+        class="mb-50 fbnt"
+        :spread="$q.screen.lt.md"
+        unelevated
+        :options="[
+          {label: 'unwrap scroll', value: 'find'},
+          {label: 'wrap scroll', value: 'create'},
+
+        ]"
+      />
+
       <div v-if="tab==='find'" class="relative-position">
         <div class="row q-col-gutter-md q-mb-lg">
           <div class="col-12 col-md-9">
-            <q-input dark  bg-color="dark" square label-color="grey-8" standout="dark text-white" class="full-width  no-border-radius" color="text-white" v-model="code" label="333-.....-...."/>
+            <q-input dark  bg-color="dark" outlined square label-color="grey-8" standout="dark text-white"
+                     class="full-width  no-border-radius" color="text-white" v-model="code" label="333..."/>
           </div>
-          <div class="col-12 col-md-3"><p class="no-margin btn-s" @click="getNote">s3arch</p></div>
+          <div class="col-12 col-md-3"><p class="no-margin btn-s" @click="getNote">unwrap</p></div>
         </div>
    <p v-if="note.text" class="bg-dark q-pa-lg text-white text-32">{{note.text}}</p>
         <div v-if="note.is_wl" class="q-mb-md ">
 
-          <q-input dark  bg-color="dark" square label-color="grey-8" standout="dark text-white" class="full-width q-mb-md  no-border-radius" color="text-white" v-model="wallet" label="Wall3t"/>
-          <q-input dark  bg-color="dark" square label-color="grey-8" standout="dark text-white" class="full-width q-mb-md no-border-radius" color="text-white" v-model="twitter" label="Twitt3r"/>
-          <q-btn @click="update" no-caps unelevated color="negative" text-color="dark" class="no-border-radius btn fbnt" label="s3nd"/>
+          <q-input dark  bg-color="dark" square outlined label-color="grey-8" standout="dark text-white" class="full-width q-mb-md
+           no-border-radius" color="text-white" v-model="wallet" label="bc1p8wdwtynk4hul2rlqgwy9lpwel94fc4684mj83lrltkf26cakr3sqh05sxw"/>
+          <q-input dark  bg-color="dark" square outlined label-color="grey-8" standout="dark text-white" class="full-width q-mb-md no-border-radius"
+                   color="text-white" v-model="twitter" label="x.com/btckeepers"/>
+          <p class="no-margin btn-s inline-block fbnt" @click="update">s3nd</p>
+
         </div>
         <div v-if="note.images?.length>0" class="row q-col-gutter-md q-mb-lg">
           <div class="col-6 col-md-3" v-for="(img,index) in note.images">
@@ -91,7 +128,7 @@
           <div class="col-12 col-md-10">  <p class="no-margin text-white text-32">333-{{cur_code}}-D3AD</p></div>
 
         </div>
-        <q-input  dark  bg-color="dark" type="textarea" square label-color="grey-8" standout="dark text-white" class="q-mb-lg full-width  no-border-radius" color="text-white" v-model="text" label="h3r3... but shhhhhhhhhhh"/>
+        <q-input  dark  bg-color="dark" type="textarea" outlined square label-color="grey-8" standout="dark text-white" class="q-mb-lg full-width  no-border-radius" color="text-white" v-model="text" label="h3r3... but shhhhhhhhhhh"/>
 
         <div class="row q-col-gutter-md q-mb-lg">
           <div class="col-6 col-md-3" v-for="(img,index) in images">
@@ -102,17 +139,18 @@
           </div>
         </div>
         <div class="flex items-center justify-between">
-          <label for="i"  class="no-border-radius btn text-white bg-dark cursor-pointer fbnt" >+add image
+          <label for="i"  class="no-border-radius btn text-24 text-dark text-sonic bg-negative cursor-pointer fbnt" >+add image
             <input id="i" accept="*/image" style="display: none" type="file" @change="addImage($event)">
           </label>
-          <q-btn @click="save" no-caps unelevated color="negative" text-color="dark" class="no-border-radius btn fbnt" label="post d3adscroll"/>
+          <p class="no-margin btn-s inline-block fbnt" @click="save">post d3adscroll</p>
+
         </div>
         <q-inner-loading :showing="loading" dark> <q-spinner size="50px" color="red" /></q-inner-loading>
       </div>
     </div>
     <div class="bottom">
       <div class="container">
-        <p class="text-64 mb-100">D3ADMAP</p>
+        <p class="text-64 mb-100 text-white">D3VSMAP</p>
         <div class="row q-col-gutter-md mb-200">
           <div class="col-12 col-md-3">
             <q-img src="~assets/yellow/1.png"/>
@@ -127,12 +165,12 @@
             <q-img src="~assets/yellow/4.png"/>
           </div>
         </div>
-        <p class="text-64 text-center">t0 b3 c0nt1nu3d...</p>
+        <p class="text-64 text-center text-negative">t0 b3 c0nt1nu3d...</p>
         <div class="text-center mb-100">
           <a href="https://twitter.com/btckeepers" target="_blank" class="q-mr-md"><img src="~assets/x.png" alt=""></a>
         </div>
         <div class="text-center">
-          <p class="text-28">BITCOINKEEPERS ©2024 | All d3ads reserved.</p>
+          <p class="text-28 text-negative">BITCOINKEEPERS ©2024 | All d3ads reserved.</p>
         </div>
       </div>
     </div>
